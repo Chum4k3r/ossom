902045290456eaf190baa2efd14d0968721bb474
Chum4k3r <joao.paes@eac.ufsm.br>
2020-06-30 15:49:52 -0300

    [DOCS] Successfully built documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-author = 'João Vitor G. Paes'
+author = 'João Vitor Gutkoski Paes'
 project = 'OsSom'
 copyright = '2020, ' + author
 
